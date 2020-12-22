9c7b46e9725c5e45a4f309cfff1eac0d3f4ec84c
antonio <antoniorocca46@gmail.com>
2020-12-22 17:14:08 +0100

    Fourth Commit

---
+++ milestone4/main.js
@@ -0,0 +1,157 @@
+let app = new Vue({
+   el: '#app',
+   data: {
+     contacts: [
+           { 
+             name: 'Michele', 
+             avatar: '_1', 
+             visible: true,
+             messages: [
+                   {
+                      date: '10/01/2020 15:30:55',
+                      text: 'Hai portato a spasso il cane?',
+                      status: 'sent'
+                    },
+                    {
+                      date: '10/01/2020 15:50:00',
+                      text: 'Ricordati di dargli da mangiare',
+                      status: 'sent'
+                    },
+                    {
+                      date: '10/01/2020 16:15:22',
+                      text: 'Tutto fatto!',
+                      status: 'received'
+                    }
+                ],
+            },
+            {
+             name: 'Fabio',
+             avatar: '_2',
+             visible: true,
+             messages: [
+                   {
+                     date: '20/03/2020 16:30:00',
+                     text: 'Ciao come stai?',
+                     status: 'sent'
+                   },
+                   {
+                     date: '20/03/2020 16:30:55',
+                     text: 'Bene grazie! Stasera ci vediamo?',
+                     status: 'received'
+                   },
+                   {
+                     date: '20/03/2020 16:35:00',
+                     text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                     status: 'sent'
+                   }
+               ],
+           },
+           {
+             name: 'Samuele',
+             avatar: '_3',
+             visible: true,
+             messages: [
+                   {
+                     date: '28/03/2020 10:10:40',
+                     text: 'La Marianna va in campagna',
+                     status: 'received'
+                   },
+                   {
+                     date: '28/03/2020 10:20:10',
+                     text: 'Sicuro di non aver sbagliato chat?',
+                     status: 'sent'
+                   },
+                   {
+                     date: '28/03/2020 16:15:22',
+                     text: 'Ah scusa!',
+                     status: 'received'
+                   }
+               ],
+           },
+           {
+             name: 'Luisa',
+             avatar: '_4',
+             visible: true,
+             messages: [
+                   {
+                     date: '10/01/2020 15:30:55',
+                     text: 'Lo sai che ha aperto una nuova pizzeria?',
+                     status: 'sent'
+                   },
+                   {
+                     date: '10/01/2020 15:50:00',
+                     text: 'Si, ma preferirei andare al cinema',
+                     status: 'received'
+                   }
+               ],
+           },
+        ],
+       contactSelected: [
+         { 
+           name: 'Michele', 
+           avatar: '_1', 
+           visible: true,
+           messages: [
+                 {
+                   date: '10/01/2020 15:30:55',
+                   text: 'Hai portato a spasso il cane?',
+                   status: 'sent'
+                 },
+                 {
+                   date: '10/01/2020 15:50:00',
+                   text: 'Ricordati di dargli da mangiare',
+                   status: 'sent'
+                 },
+                 {
+                   date: '10/01/2020 16:15:22',
+                   text: 'Tutto fatto!',
+                   status: 'received'
+                 }
+            ],
+         },
+       ],
+       newMessage: '',
+       textSearch: '',
+      },
+      methods: {
+        selezionaContatto(index) {
+          this.contactSelected = [];
+          this.index = index;
+          this.contactSelected.push(this.contacts[index]);      
+        },
+        inviaMessaggio() {
+          let nuovoMessaggio = {
+            date: this.completeDateHour(),
+            text: this.newMessage,
+            status: 'sent',
+          }
+          this.contactSelected[0].messages.push(nuovoMessaggio);
+          this.newMessage = '';
+          let replyMessage = {
+            date: this.completeDateHour(),
+            text: 'ok',
+            status: 'received',
+          }
+          setTimeout(this.contactSelected[0].messages.push(replyMessage), 1000);
+        },
+        completeDateHour() {
+          let date = new Date();
+          let day = date.getDate();
+          let month = date.getMonth() +1;
+          let year = date.getFullYear();
+          let hours = date.getHours();
+          let minutes = date.getMinutes();
+          let seconds = date.getSeconds(0);
+          return `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`
+        },
+        cercaContatto() {
+          this.contacts.forEach(contact => {
+            if (contact.name.toLowerCase().includes(textSearch.toLowerCase())) {
+                contact.visible = true;
+            } else {
+                contact.visible = false;
+            }
+          });
+        }
+     }
+});
